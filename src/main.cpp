088d9ec386f766912f76c02f9572db74ae3e4499
Omar Hassan <oh458886@gmail.com>
2024-02-27 21:42:57 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/**
+ * @brief   Firefighting Robot operates in two modes (Manual and Autonomous )
+ * @author  Group C | IEEE-RAS-ZSC 24
+ * @date    February 24, 2024
+*/
+
+/*************** Include Defentions ***************/
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <Servo.h>
+
+/*************** Macro Defentions ***************/
+
+
+
+
+/*************** Function Decleration ***************/
+
+
+/*************** Setup Application ***************/
+void setup() {
+  
+}
+
+
+/*************** Start Application ***************/
+void loop() {
+  
+}
+
+
+/*************** Function Defentions ***************/
